251656d96df051f0c592be0bbf99c5ca2cd32212
Nithishdpn <nk2457607@gmail.com>
2024-02-03 14:47:19 +0530

    ..

---
+++ database sql file/library-system.sql
@@ -0,0 +1,389 @@
+
+CREATE TABLE `author` (
+  `authorid` int(11) NOT NULL,
+  `name` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `status` enum('Enable','Disable') COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `author`
+--
+
+INSERT INTO `author` (`authorid`, `name`, `status`) VALUES
+(2, 'Alan Forbes', 'Enable'),
+(3, 'Lynn Beighley &amp; Michael Morrison', 'Enable');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book`
+--
+
+CREATE TABLE `book` (
+  `bookid` int(11) NOT NULL,
+  `categoryid` int(11) NOT NULL,
+  `authorid` int(11) NOT NULL,
+  `rackid` int(11) NOT NULL,
+  `name` text COLLATE utf8_unicode_ci NOT NULL,
+  `picture` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
+  `publisherid` int(11) NOT NULL,
+  `isbn` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `no_of_copy` int(5) NOT NULL,
+  `status` enum('Enable','Disable') COLLATE utf8_unicode_ci NOT NULL,
+  `added_on` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_on` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `book`
+--
+
+INSERT INTO `book` (`bookid`, `categoryid`, `authorid`, `rackid`, `name`, `picture`, `publisherid`, `isbn`, `no_of_copy`, `status`, `added_on`, `updated_on`) VALUES
+(1, 2, 2, 2, 'The Joy of PHP Programming', 'joy-php.jpg', 8, 'B00BALXN70', 10, 'Enable', '2022-06-12 11:12:48', '2022-06-12 11:13:27'),
+(2, 2, 3, 2, 'Head First PHP &amp; MySQL', 'header-first-php.jpg', 9, '0596006306', 10, 'Enable', '2022-06-12 11:16:01', '2022-06-12 11:16:01'),
+(3, 2, 2, 1, 'dsgsdgsd', '', 7, 'sdfsd2334', 23, 'Enable', '2022-06-12 13:29:14', '2022-06-12 13:29:14');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `categoryid` int(11) NOT NULL,
+  `name` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `status` enum('Enable','Disable') COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`categoryid`, `name`, `status`) VALUES
+(1, 'Web Design', 'Enable'),
+(2, 'Programming', 'Enable'),
+(3, 'Commerce', 'Enable'),
+(4, 'Math', 'Enable'),
+(6, 'Web Development', 'Enable');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `issued_book`
+--
+
+CREATE TABLE `issued_book` (
+  `issuebookid` int(11) NOT NULL,
+  `bookid` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `issue_date_time` datetime NOT NULL DEFAULT current_timestamp(),
+  `expected_return_date` datetime NOT NULL,
+  `return_date_time` datetime NOT NULL,
+  `status` enum('Issued','Returned','Not Return') COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `issued_book`
+--
+
+INSERT INTO `issued_book` (`issuebookid`, `bookid`, `userid`, `issue_date_time`, `expected_return_date`, `return_date_time`, `status`) VALUES
+(1, 2, 2, '2022-06-12 15:33:45', '2022-06-15 16:27:59', '2022-06-16 16:27:59', 'Issued'),
+(3, 3, 3, '2022-06-12 18:46:07', '2022-06-30 18:46:02', '2022-06-12 18:46:14', 'Returned');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `publisher`
+--
+
+CREATE TABLE `publisher` (
+  `publisherid` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `status` enum('Enable','Disable') NOT NULL
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `publisher`
+--
+
+INSERT INTO `publisher` (`publisherid`, `name`, `status`) VALUES
+(2, 'Amazon publishing', 'Enable'),
+(3, 'Penguin books ltd.', 'Enable'),
+(4, 'Vintage Publishing', 'Enable'),
+(5, 'Macmillan Publishers', 'Enable'),
+(6, 'Simon &amp; Schuster', 'Enable'),
+(7, 'HarperCollins', 'Enable'),
+(8, 'Plum Island', 'Enable'),
+(9, 'O’Reilly', 'Enable');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rack`
+--
+
+CREATE TABLE `rack` (
+  `rackid` int(11) NOT NULL,
+  `name` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `status` enum('Enable','Disable') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'Enable'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `rack`
+--
+
+INSERT INTO `rack` (`rackid`, `name`, `status`) VALUES
+(1, 'R1', 'Enable'),
+(2, 'R2', 'Enable');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(64) NOT NULL,
+  `role` enum('admin','user') DEFAULT 'admin'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `first_name`, `last_name`, `email`, `password`, `role`) VALUES
+(2, 'Mark', 'Wood', 'mark@phpzag.com', '202cb962ac59075b964b07152d234b70', 'user'),
+(3, 'George', 'Smith', 'goerge@phpzag.com', '202cb962ac59075b964b07152d234b70', 'user'),
+(4, 'Adam', NULL, 'adam@phpzag.com', '202cb962ac59075b964b07152d234b70', 'admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `author`
+--
+ALTER TABLE `author`
+  ADD PRIMARY KEY (`authorid`);
+
+--
+-- Indexes for table `book`
+--
+ALTER TABLE `book`
+  ADD PRIMARY KEY (`bookid`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`categoryid`);
+
+--
+-- Indexes for table `issued_book`
+--
+ALTER TABLE `issued_book`
+  ADD PRIMARY KEY (`issuebookid`);
+
+--
+-- Indexes for table `publisher`
+--
+ALTER TABLE `publisher`
+  ADD PRIMARY KEY (`publisherid`);
+
+--
+-- Indexes for table `rack`
+--
+ALTER TABLE `rack`
+  ADD PRIMARY KEY (`rackid`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `author`
+--
+ALTER TABLE `author`
+  MODIFY `authorid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `book`
+--
+ALTER TABLE `book`
+  MODIFY `bookid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `categoryid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `issued_book`
+--
+ALTER TABLE `issued_book`
+  MODIFY `issuebookid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `publisher`
+--
+ALTER TABLE `publisher`
+  MODIFY `publisherid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `rack`
+--
+ALTER TABLE `rack`
+  MODIFY `rackid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- Create table 'author'
+CREATE TABLE `author` (
+  `authorid` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(200) COLLATE utf8_unicode_ci NOT NULL,
+  `status` ENUM('Enable','Disable') COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`authorid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Insert data into 'author'
+INSERT INTO `author` (`authorid`, `name`, `status`) VALUES
+  (2, 'Alan Forbes', 'Enable'),
+  (3, 'Lynn Beighley & Michael Morrison', 'Enable');
+
+-- Create table 'category'
+CREATE TABLE `category` (
+  `categoryid` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(200) COLLATE utf8_unicode_ci NOT NULL,
+  `status` ENUM('Enable','Disable') COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`categoryid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Insert data into 'category'
+INSERT INTO `category` (`categoryid`, `name`, `status`) VALUES
+  (1, 'Web Design', 'Enable'),
+  (2, 'Programming', 'Enable'),
+  (3, 'Commerce', 'Enable'),
+  (4, 'Math', 'Enable'),
+  (6, 'Web Development', 'Enable');
+
+-- Create table 'publisher'
+CREATE TABLE `publisher` (
+  `publisherid` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `status` ENUM('Enable','Disable') NOT NULL,
+  PRIMARY KEY (`publisherid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Insert data into 'publisher'
+INSERT INTO `publisher` (`publisherid`, `name`, `status`) VALUES
+  (2, 'Amazon publishing', 'Enable'),
+  (3, 'Penguin books ltd.', 'Enable'),
+  (4, 'Vintage Publishing', 'Enable'),
+  (5, 'Macmillan Publishers', 'Enable'),
+  (6, 'Simon & Schuster', 'Enable'),
+  (7, 'HarperCollins', 'Enable'),
+  (8, 'Plum Island', 'Enable'),
+  (9, 'O’Reilly', 'Enable');
+
+-- Create table 'rack'
+CREATE TABLE `rack` (
+  `rackid` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(200) COLLATE utf8_unicode_ci NOT NULL,
+  `status` ENUM('Enable','Disable') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'Enable',
+  PRIMARY KEY (`rackid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Insert data into 'rack'
+INSERT INTO `rack` (`rackid`, `name`, `status`) VALUES
+  (1, 'R1', 'Enable'),
+  (2, 'R2', 'Enable');
+
+-- Create table 'user'
+CREATE TABLE `user` (
+  `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(255) DEFAULT NULL,
+  `last_name` VARCHAR(255) DEFAULT NULL,
+  `email` VARCHAR(255) DEFAULT NULL,
+  `password` VARCHAR(64) NOT NULL,
+  `role` ENUM('admin','user') DEFAULT 'admin',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Insert data into 'user'
+INSERT INTO `user` (`id`, `first_name`, `last_name`, `email`, `password`, `role`) VALUES
+  (2, 'Mark', 'Wood', 'mark@phpzag.com', '202cb962ac59075b964b07152d234b70', 'user'),
+  (3, 'George', 'Smith', 'goerge@phpzag.com', '202cb962ac59075b964b07152d234b70', 'user'),
+  (4, 'Adam', NULL, 'adam@phpzag.com', '202cb962ac59075b964b07152d234b70', 'admin');
+
+-- Create table 'book'
+CREATE TABLE `book` (
+  `bookid` INT(11) NOT NULL AUTO_INCREMENT,
+  `categoryid` INT(11) NOT NULL,
+  `authorid` INT(11) NOT NULL,
+  `rackid` INT(11) NOT NULL,
+  `name` TEXT COLLATE utf8_unicode_ci NOT NULL,
+  `picture` VARCHAR(250) COLLATE utf8_unicode_ci NOT NULL,
+  `publisherid` INT(11) NOT NULL,
+  `isbn` VARCHAR(30) COLLATE utf8_unicode_ci NOT NULL,
+  `no_of_copy` INT(5) NOT NULL,
+  `status` ENUM('Enable','Disable') COLLATE utf8_unicode_ci NOT NULL,
+  `added_on` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+  `updated_on` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+  PRIMARY KEY (`bookid`),
+  FOREIGN KEY (`authorid`) REFERENCES `author` (`authorid`),
+  FOREIGN KEY (`categoryid`) REFERENCES `category` (`categoryid`),
+  FOREIGN KEY (`rackid`) REFERENCES `rack` (`rackid`),
+  FOREIGN KEY (`publisherid`) REFERENCES `publisher` (`publisherid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Create table 'issued_book'
+CREATE TABLE `issued_book` (
+  `issuebookid` INT(11) NOT NULL AUTO_INCREMENT,
+  `bookid` INT(11) NOT NULL,
+  `userid` INT(11) NOT NULL,
+  `issue_date_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+  `expected_return_date` DATETIME NOT NULL,
+  `return_date_time` DATETIME NOT NULL,
+  `status` ENUM('Issued','Returned','Not Return') COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`issuebookid`),
+  FOREIGN KEY (`bookid`) REFERENCES `book` (`bookid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
